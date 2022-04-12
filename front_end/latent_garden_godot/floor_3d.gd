ff1a830961176cd183d0ee4fa8d2bca6bc76b39e
solcany <matus.solcany@protonmail.com>
2022-04-12 21:51:25 +0200

    floor collision detection

---
+++ front_end/latent_garden_godot/floor_3d.gd
@@ -0,0 +1,35 @@
+extends Area
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+func _physics_process(delta):	
+	var overlaps : Array = get_overlapping_bodies()
+	if !overlaps.empty():
+		print("overlaps!")
+		
+		
+#		for ov in overlaps:
+#			if(ov.kind && ov.kind == "Portal" && !is_in_portal):
+#				is_in_portal = true
+#				emit_signal("player_entered_portal")
+#	elif(is_in_portal):
+#		is_in_portal = false
+#		emit_signal("player_left_portal")
+		
