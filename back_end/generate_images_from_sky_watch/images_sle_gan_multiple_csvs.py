183fe44742492dc8a5cf48f9bbc07cdd39e9b28d
m <matus.solcany@protonmail.com>
2022-10-21 17:23:28 +0200

    generate images from multiple csvs

--- back_end/generate_images_from_sky_watch/images_sle_gan_multiple_csvs.py
+++ back_end/generate_images_from_sky_watch/images_sle_gan_multiple_csvs.py
@@ -1,49 +1,64 @@
 import numpy as np
-import sys
-import PIL
-import os
-import glob
 import tensorflow as tf
 import sle_gan
+from pathlib import Path
 import re
+import glob
+import PIL
 
-glob_data_path = '/users/m/documents/_DIGITAL/rust/sky_watch/process_sky_images_to_tiles/data/output/friday_tiles/csv/*.csv'
-data_paths = sorted(glob.glob(glob_data_path), key=lambda path: int(re.search(r'\d+', path).group()))
+GENERATOR_WEIGHTS_PATH = "./data/weights/metfaces_G-e388.h5"
+GLOB_INPUT_DATA_PATH = '/users/m/documents/_DIGITAL/rust/sky_watch/process_sky_images_to_tiles/data/output/friday_tiles/csv/*.csv'
+OUTPUT_PATH = './output/friday_images'
+IM_SHAPE = (256,256)
 
-
-imShape = (256,256)
-G_path = "./data/weights/metfaces_G-e388.h5"
-
-# latent_vectors_path = "./data/sky_watch_sample_only_positive.csv"
+def init_generator():
+    generator_shape = (1, 1, 1, IM_SHAPE[0])
+    generator = sle_gan.Generator(IM_SHAPE[0])
+    generator.build(generator_shape)
+    generator.load_weights(GENERATOR_WEIGHTS_PATH)
+    return generator
 
 def get_noise(batch_size: int):
-    return np.random.normal(loc=0.0, scale=1.0, size=(batch_size,1,1,256))
+    return np.random.normal(loc=0.0, scale=1.0, size=(batch_size,1,1,IM_SHAPE[0]))    
 
-def init_g():
-    G_shape = (1, 1, 1, imShape[0])
-    G = sle_gan.Generator(imShape[0])
-    G.build(G_shape)
-    G.load_weights(G_path)
-    return G
-
-def generate_save_img(G, vec, idx):
-    n = np.reshape(vec,(1,1,1,imShape[0]))
-    generated_image = G(n)
+def generate_image(generator, latent_point):
+    p = np.reshape(latent_point,(1,1,1,IM_SHAPE[0]))
+    generated_image = generator(p)
     generated_image = generated_image.numpy()[0]
     generated_image = ((generated_image * 127.5) + 127.5).astype(np.uint8)
-    # generated_image = generated_image.reshape(imShape)
-    im = PIL.Image.fromarray(generated_image)
-    im_path = "./output/img" + str(idx) + ".jpg"
-    im.save(im_path, format="JPEG")
+    return PIL.Image.fromarray(generated_image)
 
-#latent_vectors = np.genfromtxt(latent_vectors_path, delimiter=',')
+def main():
+    # get the csv data
+    csv_paths = sorted(glob.glob(GLOB_INPUT_DATA_PATH), key=lambda path: int(re.search(r'\d+', path).group()))
 
-print(data_paths)
+    # create the output dir
+    Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
 
-# G = init_g()
-# for idx, vec in enumerate(latent_vectors): 
-#     generate_save_img(G, vec, idx)
+    # init the generator
+    generator = init_generator()
 
-for path in data_paths:
-    print(path)
+    for csv_index, csv_path in enumerate(csv_paths):
+        print("working on csv-" + str(csv_index))
+        dir_path = OUTPUT_PATH + "/" + str(csv_index)
+        # create dir for the images
+        Path(dir_path).mkdir(parents=True, exist_ok=True)
+        # load the csv
+        latent_points = np.genfromtxt(csv_path, delimiter=',')
+
+        # generate images and save them
+        for point_index, point in enumerate(latent_points):
+            image = generate_image(generator, point)
+            image_output_path = dir_path + "/" + str(point_index) + ".jpg"
+            image.save(image_output_path, format="JPEG")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
 
