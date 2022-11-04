0e1c25eb5340c6e99329bbe9732e89f4de7f32d6
m <matus.solcany@protonmail.com>
2022-11-04 17:20:40 +0100

    add latent space nodes to scene

--- front_end/latent_garden_godot/Latent_space_node.gd
+++ front_end/latent_garden_godot/Latent_space_node.gd
@@ -1,6 +1,8 @@
+tool
+
 extends Spatial
 
-const NODE_SCALE = Vector3(0.1, 0.1, 0.1)
+const NODE_SCALE = Vector3(0.05, 0.05, 0.05)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,6 +10,9 @@ const NODE_SCALE = Vector3(0.1, 0.1, 0.1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$RigidBody.scale = NODE_SCALE
+	$RigidBody/CollisionShape.scale = NODE_SCALE
+
 	var bounding_box : Array = Geom.get_bounding_box_from_vec3(NODE_SCALE)
 	Geom.add_debug_box_to_the_scene(self, bounding_box[0], bounding_box[1])
 
