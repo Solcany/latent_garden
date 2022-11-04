83d37846b08450bc3e08443004832a504d1acee6
m <matus.solcany@protonmail.com>
2022-11-04 17:52:06 +0100

    selector follows mouse

--- front_end/latent_garden_godot/Selector.gd
+++ front_end/latent_garden_godot/Selector.gd
@@ -10,8 +10,23 @@ const SELECTOR_SCALE = Vector3(0.5, 0.5, 0.5)
 func handle_body_entered():
 	connect("body_entered", self, "_on_body_entered_selector")
 
+	
 func _on_body_entered_selector(body):
-	print("body!")
+	pass
+	#print("body!")
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseMotion:
+		print("Mouse Click/Unclick at: ", event.position)
+		var view_width = get_viewport().size.x
+		var view_height = get_viewport().size.y
+		var x : float = range_lerp(event.position.x, 0, view_width, 3, -3)
+		var y : float = range_lerp(event.position.y, 0, view_height, 3, -3)
+		self.translation = Vector3(x, y, 0)
+   #elif event is InputEventMouseMotion:
+   #	    print("Mouse Motion at: ", event.position)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,6 +37,8 @@ func _ready():
 	
 	handle_body_entered()
 	
+	
+
 
 
 
