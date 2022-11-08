43cbe7f72503402152e01e7a69b8a80fe6b4c190
m <matus.solcany@protonmail.com>
2022-11-08 12:31:32 +0100

    godot tcp client

--- front_end/latent_garden_godot/tcp_client.gd
+++ front_end/latent_garden_godot/tcp_client.gd
@@ -18,3 +18,42 @@ func connect_to_host(host: String, port: int) -> void:
 	if _stream.connect_to_host(host, port) != OK:
 		print("Error connecting to host.")
 		emit_signal("error")
+
+func send(data: PoolByteArray) -> bool:
+	if _status != _stream.STATUS_CONNECTED:
+		print("Error: Stream is not currently connected.")
+		return false
+	var error: int = _stream.put_data(data)
+	if error != OK:
+		print("Error writing to stream: ", error)
+		return false
+	return true
+
+func _process(delta: float) -> void:
+	var new_status: int = _stream.get_status()
+	if new_status != _status:
+		_status = new_status
+		match _status:
+			_stream.STATUS_NONE:
+				print("Disconnected from host.")
+				emit_signal("disconnected")
+			_stream.STATUS_CONNECTING:
+				print("Connecting to host.")
+			_stream.STATUS_CONNECTED:
+				print("Connected to host.")
+				emit_signal("connected")
+			_stream.STATUS_ERROR:
+				print("Error with socket stream.")
+				emit_signal("error")
+
+	if _status == _stream.STATUS_CONNECTED:
+		var available_bytes: int = _stream.get_available_bytes()
+		if available_bytes > 0:
+			print("available bytes: ", available_bytes)
+			var data: Array = _stream.get_partial_data(available_bytes)
+			# Check for read error.
+			if data[0] != OK:
+				print("Error getting data from stream: ", data[0])
+				emit_signal("error")
+			else:
+				emit_signal("data", data[1])
