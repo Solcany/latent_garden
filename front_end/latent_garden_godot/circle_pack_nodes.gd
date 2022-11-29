c401b1f87810fdc413b26dc90bdafcfd70f6956c
m <matus.solcany@protonmail.com>
2022-11-29 17:52:33 +0100

    flip force

--- front_end/latent_garden_godot/circle_pack_nodes.gd
+++ front_end/latent_garden_godot/circle_pack_nodes.gd
@@ -3,7 +3,7 @@ extends Spatial
 var Circle = load("res://Circle.tscn")
 
 var max_speed : float = 0.01;
-var max_force : float = 0.1;
+var max_force : float = 0.01;
 var circles : Array
 var rng = RandomNumberGenerator.new()
 
@@ -11,19 +11,35 @@ func init_circles() -> void:
 	rng.randomize()
 	for i in range(50):
 		var c = Circle.instance()
-		var pos: Vector3 = Vector3(rng.randf_range(-0.4, 0.4), rng.randf_range(-0.4, 0.4), 0)
-		c.init(pos, 0.2)
+		var pos: Vector3 = Vector3(rng.randf_range(-0.2, 0.2), rng.randf_range(-0.2, 0.2), 0)
+		c.init(pos, 0.1)
 		c.add_to_group("circles")
 		self.add_child(c)	
 	
 func set_vec_length(vec : Vector3 , new_length: float) -> Vector3:
 	return vec * new_length / vec.length()
 
-func check_circle_position(circle_i : int, circles : Array) -> void:
-	var circle = circles[circle_i]
-	for i in range(circle_i+1, circles.size()):
+
+func check_borders(i : int, circles: Array) -> void:
+	var the_circle= circles[i]
+	if (the_circle.translation.x-the_circle.radius < -1 or the_circle.translation.x+the_circle.radius > 1):
+	  the_circle.velocity.x*=-1
+	  the_circle.update()
+	
+	if (the_circle.translation.y-the_circle.radius < -1 or the_circle.translation.y+the_circle.radius > 1):
+	  the_circle.velocity.y*=-1
+	  the_circle.update()	
+
+
+func check_circle_position(i : int, circles : Array) -> void:
+	var circle = circles[i]
+	for j in range(i+1, circles.size()+1):
 		var count = 0
-		var other_circle = circles[i]
+		var other_circle : Spatial
+		if(j == circles.size()):
+			other_circle = circles[0]
+		else:
+			other_circle = circles[j]
 		var dist = circle.translation.distance_to(other_circle.translation)
 		if(dist < circle.radius + other_circle.radius):
 			count += 1
@@ -36,26 +52,19 @@ func get_separation_forces(circle1, circle2) -> Vector3:
 	var dist : float = circle1.translation.distance_to(circle2.translation)
 	if( dist > 0 and dist < circle1.radius + circle2.radius):
 		var diff : Vector3 = circle1.translation - circle2.translation
-		diff = diff.normalized()
+		diff.normalized()
 		diff /= dist
 		steer += diff
 	return steer
 	
-func apply_separation_forces_to_circle(i: int, circles : Array) -> void:
-	var separate_forces : Array = []
-	var near_circles : Array = []
-	for _i in range(circles.size()):
-		separate_forces.append(Vector3(0,0,0))
-		near_circles.append(0)
-		
+func apply_separation_forces_to_circle(separate_forces: Array, near_circles: Array, i: int, circles : Array) -> void:
 	var the_circle = circles[i]
-
 	for j in range(i+1, circles.size()):
 		var other_circle = circles[j]
 		var force_ij : Vector3 = get_separation_forces(the_circle, other_circle)
 		if(force_ij.length() > 0):
 			separate_forces[i] += force_ij        
-			separate_forces[j] += force_ij
+			separate_forces[j] -= force_ij
 			near_circles[i] += 1
 			near_circles[j] += 1
 	if (near_circles[i] > 0):
@@ -74,6 +83,12 @@ func _ready():
 	circles = get_tree().get_nodes_in_group("circles")
 
 func _process(delta):
-  for i in range(circles.size()):
-	  check_circle_position(i, circles)
-	  apply_separation_forces_to_circle(i, circles);
+	var separate_forces : Array = []
+	var near_circles : Array = []
+	for _i in range(circles.size()):
+		separate_forces.append(Vector3(0,0,0))
+		near_circles.append(0)
+	for i in range(circles.size()):
+		check_borders(i, circles)
+		check_circle_position(i, circles)
+		apply_separation_forces_to_circle(separate_forces, near_circles, i, circles)
