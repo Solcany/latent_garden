152753cfae04fc3f191c299581eb958788b3f4d9
m <matus.solcany@protonmail.com>
2022-12-13 17:14:21 +0100

    slerp list

--- back_end/slerp/slerp.py
+++ back_end/slerp/slerp.py
@@ -9,36 +9,46 @@ def slerp(val, low, high):
 		return (1.0-val) * low + val * high
 	return np.sin((1.0-val)*omega) / so * low + np.sin(val*omega) / so * high
 
-def get_point():
+def get_points():
+	latent_dim = 2
+	n_samples = 2
 	# generate points in the latent space
-	x_input = np.random.randn(1)
+	x_input = np.random.randn(latent_dim * n_samples)
 	# reshape into a batch of inputs for the network
-	z_input = x_input.reshape(1, 1)
+	z_input = x_input.reshape(n_samples, latent_dim)
 	return z_input
 
-def slerp_list(pts, steps):
-	new_pts = []
-	ratios = np.linspace(0, 1, num=steps)	
-	for idx in range(len(pts)-1):
-		pt1 = pts[idx]
-		pt2 = pts[idx+1]
-		new = slerp(ratios, pt1, pt2)
-		#drop last slerped vector to avoid duplication
-		last_idx = len(new)-1
-		new = new[:last_idx]
-		new_pts.append(new)
+def slerp_list(vectors, steps):
+	slerped_vectors = []
+	ratios = np.linspace(0, 1, num=steps)
+	# remove the last ratio = 1.0 to avoid duplicate vectors
+	ratios = ratios[:len(ratios)-1]
+	for idx in range(len(vectors)-1):
+		vec1 = vectors[idx]
+		vec2 = vectors[idx+1]
+		for ratio in ratios:
+			new_vec = slerp(ratio, vec1, vec2)
+			slerped_vectors.append(new_vec)
 	# add the very last point
-	new_pts.append(pts[-1])
-	print(new_pts)
-
-def slerp_test():
-	v1 = get_point()
-	v2 = get_point()
-	ratios = np.linspace(0, 1, num=3)
-	print(slerp(np.array([0.5]), v1, v2))
+	slerped_vectors.append(vectors[-1])
+	print(slerped_vectors)
 
 def main():
-	slerp_list([np.array(1.0), np.array(2.0), np.array(3.0)], 3)
+	data = [np.array([1.0, 1.0]), 
+			np.array([2.0, 2.0]), 
+			np.array([3.0, 3.0])]
+	#ratios = np.linspace(0, 1, num=4)
+
+	#points = get_points()	
+
+	slerp_list(data, 4)
+
+	# for ratio in ratios:
+	# 	print(slerp(ratio, data[0], data[1]))
+
+
+
+	#slerp_list(data, 3)
 
 if __name__ == '__main__':
     main()
