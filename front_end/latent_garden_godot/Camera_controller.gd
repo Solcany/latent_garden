0f4043f7681a09661085b96617d2bd020b59bf69
m <matus.solcany@protonmail.com>
2023-01-16 15:15:29 +0100

    pan camera on dragg

--- front_end/latent_garden_godot/Camera_controller.gd
+++ front_end/latent_garden_godot/Camera_controller.gd
@@ -4,6 +4,8 @@ var is_camera_moving : bool = false
 var mouse_pos : Vector2 = Vector2(0,0)
 var viewport_size : Vector2
 var viewport_center : Vector2
+var is_mouse_pressed : bool = false
+var prev_norm_mouse_pos : Vector2 = Vector2(0,0)
 
 func _on_mouse_movement(event) -> void:
 	var mouse_x : float = event.position.x
@@ -26,6 +28,33 @@ func handle_camera_moving() -> void:
 		var y : float = sin(angle) * Constants.CAMERA_LATERAL_MOVEMENT_DAMPING
 		var dir : Vector3 = Vector3(x, y, 0);
 		$Camera.translation += dir
+
+func handle_mouse_moving(event) -> void:
+	if(is_mouse_pressed):
+		var mouse_norm_pos : Vector2
+		
+		mouse_norm_pos.x = event.position.x / viewport_size.x * 2.0 - 1.0 # normalise & remap to -1.0, 1.0 range
+		mouse_norm_pos.y = event.position.y / viewport_size.y * 2.0 - 1.0 # normalise & remap to -1.0, 1.0 range
+		
+		var screen_travel : Vector2 = mouse_norm_pos - prev_norm_mouse_pos
+		
+		var new_translation : Vector3
+		new_translation.x = - screen_travel.x * (viewport_size.x/viewport_size.y)
+		new_translation.y = screen_travel.y
+		new_translation.z = 0.0
+		
+		$Camera.translation += new_translation
+		
+		prev_norm_mouse_pos = mouse_norm_pos
+	else:
+		prev_norm_mouse_pos.x = event.position.x / viewport_size.x * 2.0 - 1.0 # normalise & remap to -1.0, 1.0 range
+		prev_norm_mouse_pos.y = event.position.y / viewport_size.y * 2.0 - 1.0 # normalise & remap to -1.0, 1.0 range
+
+func handle_mouse_pressed(event) -> void:
+	if(event.pressed):
+		is_mouse_pressed = true
+	else:
+		is_mouse_pressed = false
 		
 # WIP this handler funcand the event should be renamed to more generic name, 
 # change in App and the slider
@@ -43,5 +72,9 @@ func _process(delta):
 	
 func _input(event):
 	if event is InputEventMouseMotion:
-		_on_mouse_movement(event)
+		handle_mouse_moving(event)
+		
+	if event is InputEventMouseButton:
+		handle_mouse_pressed(event)
+		
 	
