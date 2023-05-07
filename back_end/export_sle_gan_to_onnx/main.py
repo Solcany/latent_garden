c8ef7769e774d11ea68e9fc83730454265f63fb6
m <matus.solcany@protonmail.com>
2023-05-07 12:40:09 +0200

    rm comments

--- back_end/export_sle_gan_to_onnx/main.py
+++ back_end/export_sle_gan_to_onnx/main.py
@@ -3,17 +3,6 @@ import tensorflow as tf
 import tf2onnx
 import onnx
 
-# model = tf.keras.Sequential()
-# model.add(tf.keras.layers.Dense(4, activation="relu"))
-
-# input_signature = [tf.TensorSpec([3, 3], tf.float32, name='x')]
-# # Use from_function for tf functions
-# onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature, opset=13)
-# onnx.save(onnx_model, "dst/path/model.onnx")
-
-
-
-
 GAN_WEIGHTS_PATH = "./data/gan/metfaces_G-e388.h5"
 IMAGE_SHAPE = 256
 MODEL_OUT_PATH = "./data/export/metfaces_sle_gan.onnx"
@@ -25,10 +14,6 @@ def main():
 	input_signature = [tf.TensorSpec([1, 1, 1, IMAGE_SHAPE], tf.float32, name='x')]
 	onnx_model, _ = tf2onnx.convert.from_keras(gan, input_signature, opset=13)
 	onnx.save(onnx_model, MODEL_OUT_PATH)
-
-	# input_noise = sle_gan.create_input_noise(batch_size=1)
-	# gan.predict(input_noise)
-	#gan.save(MODEL_OUT_PATH)
-
+	
 if __name__ == '__main__':
     main()
